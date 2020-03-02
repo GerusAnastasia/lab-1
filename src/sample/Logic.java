bac3ad06f34e18eb69bb4c9581aa4185dfb33bf8
Asik666 <57961135+Asik666@users.noreply.github.com>
2020-03-02 09:09:58 +0300

    laba-1

---
+++ src/sample/Logic.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class Logic {
+    static int Addition(int n1, int n2)
+    {
+       int rezult = n1+n2;
+       return rezult;
+    }
+
+    static int Multiplication(int n1, int n2)
+    {
+        int rezult = n1 * n2;
+        return rezult;
+    }
+
+
+}
